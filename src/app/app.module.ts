8d5fe4a011457ea27f0ae5a89b9b92beae453e15
amrelkoumy <amr.m.elkoumy@gmail.com>
2022-07-11 17:09:29 +0200

    add  dashboard page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,30 +5,21 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
-<<<<<<< HEAD
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { UsersComponent } from './components/users/users.component';
 import { SellersComponent } from './components/sellers/sellers.component';
 import { ProductsComponent } from './components/products/products.component';
 import { UsersService } from './services/users.service';
 import { HttpClientModule } from '@angular/common/http';
-=======
-import { HomeComponent } from './components/home/home.component';
-
->>>>>>> AmrElkoumy
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
     HeaderComponent,
-<<<<<<< HEAD
     DashboardComponent,
     UsersComponent,
     SellersComponent,
     ProductsComponent,
-=======
-    HomeComponent
->>>>>>> AmrElkoumy
   ],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [UsersService],
