8411e98d63766078b0c352ed193667d085221e5a
omar <ommrahmdd@gmail.com>
2022-07-11 19:35:00 +0200

    .

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { Router } from '@angular/router';
 import { IUser } from 'src/app/models/IUser';
 import { UsersService } from 'src/app/services/users.service';
@@ -8,14 +8,16 @@ import { UsersService } from 'src/app/services/users.service';
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.scss'],
 })
-export class UsersComponent implements OnInit {
+export class UsersComponent implements OnInit, OnChanges {
   users: IUser[] = [];
   constructor(private userServ: UsersService, private router: Router) {}
 
   ngOnInit(): void {
     this.getUsers();
   }
-
+  ngOnChanges(): void {
+    this.getUsers();
+  }
   getUsers() {
     this.userServ.getUsers().subscribe((data) => {
       this.users = data;
@@ -23,5 +25,22 @@ export class UsersComponent implements OnInit {
   }
   addNewUser() {
     this.router.navigateByUrl('users/newUser');
+  }
+  deleteUser(userId: number) {
+    let Is_Deleted = confirm('Are Your sure you want to delete this user ?');
+    if (Is_Deleted) {
+      this.userServ.deleteUser(userId).subscribe((data) => {
+        console.log(data);
+      });
+    }
+  }
+
+  getUser(query: any) {
+    query = isNaN(query) ? query : Number(query);
+    console.log('type of ', typeof query);
+    this.userServ.getUser(query).subscribe((data) => {
+      console.log(typeof data);
+      console.log(data);
+    });
   }
 }
--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -11,4 +11,35 @@ export class UsersService {
   getUsers(): Observable<IUser[]> {
     return this.http.get<IUser[]>('http://localhost:3000/users');
   }
+  deleteUser(userId: number) {
+    return this.http.delete<IUser>(`http://localhost:3000/users/${userId}`);
+  }
+  getUser(query?: any) {
+    if (isNaN(query) && query.includes('.com')) {
+      console.log('.com');
+      return this.http.get<IUser[] | IUser>(
+        `http://localhost:3000/users?email=${query}`
+      );
+    } else if (query.length === 9 && typeof query === 'number') {
+      console.log('National');
+      return this.http.get<IUser>(
+        `http://localhost:3000/users?national_id=${query}`
+      );
+    } else if (
+      isNaN(query) &&
+      !query.includes('.com') &&
+      typeof query != 'number' &&
+      query != ''
+    ) {
+      console.log('name');
+      return this.http.get<IUser[]>(
+        `http://localhost:3000/users?name=${query}`
+      );
+    } else if (query == '') {
+      console.log('enm');
+      return this.http.get<IUser[]>('http://localhost:3000/users');
+    }
+    console.log('return');
+    return this.http.get<IUser[]>('http://localhost:3000/users');
+  }
 }
